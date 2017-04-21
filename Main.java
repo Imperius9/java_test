c537a79fd0496023fb613b4180d07586ac6bc005
Imperius9 <m18560669819@163.com>
2017-04-21 19:49:28 +0800

    java-test and commit test

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+class tests{
+    public static Boolean isNumeric(String str){
+        Pattern pattern=Pattern.compile("[0-9]*");
+        Matcher isNum=pattern.matcher(str);
+        if(!isNum.matches()){
+            return false;
+        }
+        return true;
+    }
+}
+
+public class Main {
+
+    public  static void main(String[] args) {
+        //double x=1;
+        //double y=2;
+        //for (Operation operation : Operation.values()){
+        //    System.out.print( x  );
+        //    System.out.print( operation );
+        //    System.out.print( y );
+        //    System.out.println("=" + operation.apply(x,y));
+        //}
+        //Operation op = Operation.PLUS;
+        //System.out.println(op.apply(1,2));
+        System.out.println(Week.THU.value);
+    }
+}
+
---
+++ Week.java
@@ -0,0 +1,11 @@
+/**
+ * Created by 花间一壶酒 on 2017/3/21.
+ */
+public enum Week {
+    Mon(1),TUE(2),WED(3),THU(4),FRI(5),SAT(6),SUN(7);
+    int value;
+    Week(int i){
+        this.value=i;
+    }
+
+}
